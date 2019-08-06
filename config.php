4979eb26ee2406641b9dd2254f6e494bcf0d41fa
beichixing <910713290@qq.com>
2019-08-06 15:25:18 +0800

    beat2

--- config.php
+++ config.php
@@ -7,12 +7,21 @@ $cfg_dbpwd = '';	//数据库密码
 $cfg_db_language = 'utf8'; 	//数据库编码
 $date_db = date("Y-m-d H:i:s");
 $sql_name = $date_db."-".$cfg_dbname.".sql";
-$to_file_name = 'buck_log/'.$date_db."-".$cfg_dbname.".sql"; //备份文件命名规则
+$to_file_name = 'back_log/'.$date_db."-".$cfg_dbname.".sql"; //备份文件命名规则
 $cfg_type = '1'; 	//	备份方式：1.邮箱备份 2. 阿里云oss备份
 
 
 //////////////////////////邮箱备份设置\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 $cfg_email = '';	// 接收备份的邮箱  请将 imhzs@sina.com 设置为收信白名单
+
+$email_info = [
+	'Host' => '',  // 发件邮箱的SMTP服务器地址
+	'Username' => '', //发件邮箱的用户名
+	'Password' => '',  //发件邮箱的密码（或授权码）
+	'SMTPSecure' => 'ssl', //发件邮箱的认证方式
+	'Port' => '465' //发件邮箱端口
+
+];
 
 
 ////////////////////////阿里云oss备份设置\\\\\\\\\\\\\\\\\\\\\\\\\\\\
